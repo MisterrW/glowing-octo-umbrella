61569c357b7212e96fd20cc441fe29fbea9f2a8e
William Robertson <w.robertson305@gmail.com>
2016-12-05 16:20:29 +0000

    jar

--- Runner.java
+++ Runner.java
@@ -1,6 +1,6 @@
 class Runner {
   public static void main (String[] args) {
-    Container container = new Container("Jeff");
+    Container container = new Container("jar");
     for (int i = 0; i < 8; i++) {
       Item item = new Item();
       container.addToContents(item);
